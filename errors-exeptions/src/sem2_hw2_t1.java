fa40aec19cd5738a200ed99a8a50c7e2d2ce5ab4
Danny_D <tip505@mail.ru>
2023-09-03 18:43:53 +0300

    first_task_done

---
+++ errors-exeptions/src/sem2_hw2_t1.java
@@ -0,0 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class sem2_hw2_t1 {
+    public static void main(String[] args) {
+        float number = getOnlyFloatNumber();
+        System.out.println("Введённое число = "+ number);
+    }
+
+    public static <inputMismatchException> float getOnlyFloatNumber(){
+        Scanner scanner = new Scanner(System.in);
+        float num = 0f;
+        boolean flag = true;
+        while (flag){
+            try {
+                System.out.println("Введите дробное число через (,): ");
+                num = scanner.nextFloat();
+                flag = false;
+            } catch (InputMismatchException e) {
+                    System.out.println("Введено не дробное число, или использован неверный разделитель!\n");
+                    scanner.next();
+            }
+
+        }
+        return num;
+    }
+}
