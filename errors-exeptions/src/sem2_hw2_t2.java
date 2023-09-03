10223eb19a76fd8040a2a98828b6b5942b4d7d56
Danny_D <tip505@mail.ru>
2023-09-03 20:13:34 +0300

    task_done

--- errors-exeptions/src/sem2_hw2_t2.java
+++ errors-exeptions/src/sem2_hw2_t2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class sem2_hw2_t2 {
+//Задание 2
+//        try {
+//        int d = 0;
+//        double catchedRes1 = intArray[8] / d;
+//        System.out.println("catchedRes1 = " + catchedRes1);
+//        } catch (ArithmeticException e) {
+//        System.out.println("Catching exception: " + e);
+//        }
+
+
+public class sem2_hw2_t2 {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            int intArray = 8;/*введена переменная для исправления ошибки "cannot find symbol"*/
+            double catchedRes1 = intArray / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e.getMessage());/*добавлен метод getMessage для оптимизации вывода*/
+        }
+    }
+
+
 }
--- errors-exeptions/src/sem2_hw2_t3.java
+++ errors-exeptions/src/sem2_hw2_t3.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class sem2_hw2_t3 {
+public class sem2_hw2_t3 {
+    public static void main(String[] args) throws IndexOutOfBoundsException /*только данное исключение ожидается от нашего метода при нормальной работе системы и условнии неизменности кода*/{
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        }  /*catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } - данное исключение не может быть воспроизведено в предложенном коде (принимается во внимание, что код не изменяем)*/
+        catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+//        catch (Throwable ex) {
+//            System.out.println("Что-то пошло не так...");/*Необходимо поставить в 3-ю очередь, т.к. это базовый тип исключения и он будет "перехватывать" остальные
+//            , также необходимо учесть, что это очень обширное исключение и в данном коде его использование под сомнением, лучше даже удалить*/
+
+    }
+    public static void printSum(Integer a, Integer b) /*throws FileNotFoundException - такое исключение невозможно в данном методе*/ {
+        System.out.println(a + b);
+    }
+
 }
